ede5c82bc62b41ac9214bc1863e4d64bc42fb95a
cirko0 <cirko007ivan@gmail.com>
2026-01-16 20:09:50 +0100

    add Dogstudio-style footer clone

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,35 +1,86 @@
-import {
-  FaInstagram,
-  FaFacebook,
-  FaXTwitter,
-  FaDribbble,
-} from "react-icons/fa6";
+import flagIcon from "../assets/download.jpg";
+import logo from "../assets/baseline.png";
+import mail from "../assets/mail.png";
+import carret from "../assets/carret.png";
 
-const Footer = () => {
-  const year = new Date().getFullYear();
+const cities = ["Chicago", "Amsterdam", "Paris"];
 
+const socials = [
+  { label: "Fb", href: "https://www.facebook.com/dogstudio/" },
+  { label: "Ins", href: "https://www.instagram.com/dogstudio.co/" },
+  { label: "Dri", href: "https://dribbble.com/dogstudio" },
+  { label: "Tw", href: "https://twitter.com/Dogstudio" },
+];
+
+export default function Footer() {
   return (
-    <footer className="footer">
-      <p>&copy; {year} DogStudio Clone. All rights reserved.</p>
+    <footer className="site-footer js-in-view in-view">
+      <div className="center">
+        {/* ===== TOP ===== */}
+        <div className="site-footer-top">
+          {cities.map((city, i) => (
+            <div
+              key={city}
+              className={`site-footer-block site-footer-block--address appear-fade-up appear-delay--${
+                i + 1
+              }`}
+            >
+              <h3>
+                {city} <b>.</b>
+              </h3>
+            </div>
+          ))}
 
-      <div></div>
+          <div className="site-footer-block site-footer-block--social appear-fade-up appear-delay--3">
+            {/* Put big logo SVG in separate component */}
+            <h2 className="site-footer-title">
+              <img className="baseline" src={logo} alt="Dogstudio logo" />
+            </h2>
 
-      <div className="icons-container">
-        <a href="https://instagram.com/" target="_blank">
-          <FaInstagram />
-        </a>
-        <a href="https://facebook.com/" target="_blank">
-          <FaFacebook />
-        </a>
-        <a href="https://x.com/" target="_blank">
-          <FaXTwitter />
-        </a>
-        <a href="https://dribbble.com/" target="_blank">
-          <FaDribbble />
-        </a>
+            <ul>
+              {socials.map((s) => (
+                <li key={s.label}>
+                  <a href={s.href} target="_blank">
+                    {s.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        {/* ===== BOTTOM ===== */}
+        <div className="site-footer-bottom">
+          <p className="site-footer-contact appear-fade-up">
+            We'd love to hear from you{" "}
+            <a href="mailto:biz@dogstudio.be">biz@dogstudio.be</a>
+          </p>
+
+          <div className="site-footer-right">
+            <p className="site-footer-newsletter appear-fade-up appear-delay--4">
+              <a href="#">
+                <img className="svg svg-mail" src={mail} />
+                Subscribe to our newsletter
+              </a>
+            </p>
+
+            <div className="site-footer-languages appear-fade-up appear-delay--5">
+              Language:
+              <ul>
+                <li className="is-active">
+                  <a href="#">
+                    <img src={flagIcon} />
+                    English
+                    <span className="svg svg-caret-right">
+                      <img src={carret} />
+                    </span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </footer>
   );
-};
-
-export default Footer;
+}
